23b4ad4289eb3af889125e5a51f9769a0012a015
zunghun <61766458+zunghun@users.noreply.github.com>
2024-01-05 12:45:32 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -48,21 +48,13 @@ try:
                 #MainMenu {visibility: hidden;}
                 footer {visibility: hidden;}
                 header {visibility: hidden;}
+                
+                #GithubIcon {
+                visibility: hidden;
                 </style>
                 """
                 st.markdown(hide_st_style, unsafe_allow_html=True) 
-                st.markdown(
-                """
-                <style>
-                .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-                .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-                .viewerBadge_text__1JaDK {
-                    display: none;
-                }
-                </style>
-                """,
-                unsafe_allow_html=True
-                )  
+                  
                 st.markdown(
                     """
                     <style>
