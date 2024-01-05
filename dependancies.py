6d075dcbc49d6d8d053b0b3369f53431267a76c5
zunghun <61766458+zunghun@users.noreply.github.com>
2024-01-05 17:07:32 +0900

    Add files via upload

--- dependancies.py
+++ dependancies.py
@@ -6,7 +6,7 @@ import re
 from deta import Deta
 from dotenv import load_dotenv
 
-load_dotenv(".env")
+# load_dotenv(".env")
 # DETA_KEY = os.getenv("DETA_KEY")
 DETA_KEY = 'c0BhxUXtVCm_cxyhUVYPtmQCesY4u7hU1ym89dpzH7gJ'
 
