5ec56e37568b09b2c0d1ce987e48dd4aef9bb761
zunghun <61766458+zunghun@users.noreply.github.com>
2024-01-23 12:41:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,7 @@ from dependancies import sign_up, fetch_users
 import pandas as pd
 import geopandas as gpd
 import plotly.express as px
-from st_files_connection import FilesConnection
 
-# # Create connection object and retrieve file contents.
-# # Specify input format is a csv and to cache the result for 600 seconds.
-# conn = st.connection('s3', type=FilesConnection)
-# df = conn.read("testbucket-jrieke/myfile.csv", input_format="csv", ttl=600)
-
-# # Print results.
-# for row in df.itertuples():
-#     st.write(f"{row.Owner} has a :{row.Pet}:")
     
     
 
